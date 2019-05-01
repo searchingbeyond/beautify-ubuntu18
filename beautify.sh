f6fa4df2a88d76e355f04e73f9f4ab912f604300
searchingbeyond <1355036599@qq.com>
2019-05-01 10:12:00 +0800

    增加美化工作的脚本

---
+++ beautify.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+# 美化ubuntu18, 切换为root身份
+
+# install gnome
+sudo apt-get install -y gnome-tweak-tool
+sudo apt-get install -y gnome-shell-extensions
+sudo apt-get install -y gnome-shell-extension-dashtodock
+sudo apt-get install -y wine-stable
+
+# download resouces
+cd /usr/local/src
+git clone 
+cd beautify-ubuntu18
+
+mv themes_icons/Sierra* /usr/local/share/themes
+mv themes_icons/MacOSX* /usr/local/share/icons
+
+# install themes
+cd /usr/local/share/themes
+xz -d Sierra-dark.tar.xz
+tar xf Sierra-dark.tar
+xz -d Sierra-light.tar.xz
+tar xf Sierra-light.tar
+
+# install icons
+cd /usr/local/share/icons
+xz -d MacOSX-dark-icon-theme.tar.xz
+tar xf MacOSX-dark-icon-theme.tar
+xz -d MacOSX-icon-theme.tar.xz
+tar xf MacOSX-icon-theme.tar
+
+# start to install applications
+cd /usr/local/src/beautify-ubuntu18/applications
+
+# install chrome browser
+dpkg -i google-chrome-stable_current_amd64.deb
+
+# install vs code
+dpkg -i code_1.33.0-1554390824_amd64.deb
+
+# install sogou input
+dpkg -i sogoupinyin_2.2.0.0108_amd64.deb
+# resolve dependency
+apt-get install -f -y
+# install again
+dpkg -i sogoupinyin_2.2.0.0108_amd64.deb
+
+# install shadowsocks
+cp shadowsocks /usr/local/shadowsocks
+# add desktop icons
+cp -r desktops/*.desktop /usr/share/applications
+
